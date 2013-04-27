a45f7d375c4e05a2ea789ab12cd254f2e9b9251e
ruanhao <ryan.ruan@ericsson.com>
2013-04-27 11:04:30 +0800

    update turtle dbgecho using caller

--- turtle.sh
+++ turtle.sh
@@ -4,21 +4,30 @@ clear
 
 dbgecho() 
 {
-    printf "#%-30s @%-8s --- $1\n" $3 $2
-    #echo "#$3, @line:$2 --- $1"
+    ## printf "#%-30s @%-8s --- $1\n" $3 $2
+    ## echo "#$3, @line:$2 --- $1"
+    a=$( caller 0 )
+    OldIFS="$IFS"
+    IFS=" "
+    arr=( $a )
+    IFS="$OldIFS"
+    local line=${arr[0]}
+    local func=${arr[1]}
+    printf "#%-30s @%-8s --- $1\n" $func $line
+
 }
 
 test_local()
 {
     local a
     a=12345
-    dbgecho "local a: $a" $LINENO $FUNCNAME
+    dbgecho "local a: $a"
 }
 
 test_background_loop() 
 {
     for i in "a" "b" "c"; do
-        dbgecho "background loop: $i" $LINENO $FUNCNAME
+        dbgecho "background loop: $i"
     done &
 }
 
@@ -33,13 +42,19 @@ test_brace_redirection()
         echo "world"
         echo "haha between braces"
     }>$file
-    dbgecho "write ok" $LINENO $FUNCNAME
+    dbgecho "write ok"
     {
         read l1 ## should not write as read $l1
         read l2
         read l3
     }<$file
-    dbgecho "$l1, $l2, $l3" $LINENO $FUNCNAME
+    dbgecho "$l1, $l2, $l3"
+    ## it also works like below: 
+    while read temp; do
+        dbgecho "in while read: $temp"
+    done<$file
+
+    rm -f $file
 }
 
 help()
@@ -59,35 +74,35 @@ test_boolean_and_parameter()
 {
     file="/bin/vi"
     if [ -f "$file" ]; then
-        dbgecho "$file exists" $LINENO $FUNCNAME
+        dbgecho "$file exists" 
     fi
 
     if [ "$file" = "/bin/vi" ]; then
-        dbgecho "two virables equal" $LINENO $FUNCNAME
+        dbgecho "two virables equal" 
     fi
 
     ## clause between {} is called 'anonymous function'
-    [ -f "$file" ] && dbgecho "$file exists" $LINENO $FUNCNAME || \
-        { dbgecho "$file does not exists" $LINENO $FUNCNAME; exit 1; } 
+    [ -f "$file" ] && dbgecho "$file exists" || \
+        { dbgecho "$file does not exists"; exit 1; } 
 
-    [[ -n "$1" ]] && dbgecho "\$1 is defined: $1" $LINENO $FUNCNAME || \
-        dbgecho "\$1 is not defined" $LINENO $FUNCNAME
-    [ -n "$2" ] && dbgecho "\$2 is defined: $2" $LINENO $FUNCNAME || \
-        dbgecho "\$2 is not defined" $LINENO $FUNCNAME
+    [[ -n "$1" ]] && dbgecho "\$1 is defined: $1" || \
+        dbgecho "\$1 is not defined"
+    [ -n "$2" ] && dbgecho "\$2 is defined: $2" || \
+        dbgecho "\$2 is not defined"
 
     local nArgs=$#
-    dbgecho "number of args: $nArgs" $LINENO $FUNCNAME
+    dbgecho "number of args: $nArgs"
     local lastArg=${!nArgs}    # ${!#} is prefered :)
-    dbgecho "last arg: $lastArg" $LINENO $FUNCNAME
+    dbgecho "last arg: $lastArg"
 
     local enter="/root/"
     if cd $enter 2> /dev/null; then
-        dbgecho "enter new dir" $LINENO $FUNCNAME
+        dbgecho "enter new dir"
         for item in $enter/*; do    ## take a look at the usage: $enter/*
-            dbgecho "item: $item" $LINENO $FUNCNAME
+            dbgecho "item: $item"
         done
     else
-        dbgecho "entering dir fails" $LINENO $FUNCNAME
+        dbgecho "entering dir fails"
     fi
 
 
@@ -99,25 +114,25 @@ test_loop()
     if [ -f $zipFile ]; then 
         case $( file $zipFile ) in 
             *"Zip archive"* )
-                dbgecho "zip archive" $LINENO $FUNCNAME;;
+                dbgecho "zip archive";;
             *"gzip compressed"* )
-                dbgecho "gzip archive" $LINENO $FUNCNAME;;
+                dbgecho "gzip archive";;
             *"bzip2 compressed"* ) 
-                dbgecho "bzip2 archive" $LINENO $FUNCNAME;;
+                dbgecho "bzip2 archive";;
             * ) 
-                dbgecho "undef" $LINENO $FUNCNAME;;
+                dbgecho "undef";;
         esac
     else 
-        dbgecho "$zipFile does not exists" $LINENO $FUNCNAME
+        dbgecho "$zipFile does not exists"
     fi
 
     ## {1..3} can be replaced by "1" "2" "3"
     for i in {1..3}; do 
-    dbgecho "$i" $LINENO $FUNCNAME
+    dbgecho "$i"
     done
 
     for (( i=0;i<3;i++ )); do
-        dbgecho "$i" $LINENO $FUNCNAME
+        dbgecho "$i"
     done
 }
 
@@ -137,52 +152,52 @@ test_loop()
 
 test_algorithm() 
 {
-    dbgecho $(( 2#101010 )) $LINENO $FUNCNAME
+    dbgecho $(( 2#101010 ))
 
     ## ',' links a series of math op, last op is returned
-    dbgecho $(( 1+2,2+3 )) $LINENO $FUNCNAME
+    dbgecho $(( 1+2,2+3 ))
 }
 
 test_string() 
 {
     local str="hello world"
-    dbgecho "length of str: ${#str}" $LINENO $FUNCNAME
+    dbgecho "length of str: ${#str}"
 
-    dbgecho "substr to end: ${str:6}" $LINENO $FUNCNAME
-    dbgecho "slice substr: ${str:6:3}" $LINENO $FUNCNAME    ## the same as: echo | awk '{printf substr("'"$str"'", 3, 4) "\n"}'
-                                                            ## take note: echo is used to generate a fake input so as not to offer a file to awk
-                                                            ## take note: "'"$str"'"
-    dbgecho "substr from backend: ${str:(-6)}" $LINENO $FUNCNAME
-    dbgecho "subtract paras to the end (*): ${*:3}" $LINENO $FUNCNAME  
-    ## dbgecho "subtract para (@): ${@:3}" $LINENO $FUNCNAME ## look at the difference in the stdout :) 
-    dbgecho "subtract paras like slice: ${*:3:2}" $LINENO $FUNCNAME  
-    dbgecho "cut str from left (not greedy): ${str#h*l}" $LINENO $FUNCNAME  
-    dbgecho "cut str from left (greedy): ${str##h*l}" $LINENO $FUNCNAME  
-    dbgecho "cut str from right (not greedy): ${str%l*}" $LINENO $FUNCNAME  
-    dbgecho "cut str from right (greedy): ${str%%l*}" $LINENO $FUNCNAME  
-    dbgecho "replace str once: ${str/l/o}" $LINENO $FUNCNAME  
-    dbgecho "replace str all: ${str//l/o}" $LINENO $FUNCNAME  
-    dbgecho "replace str from head: ${str/#he/she}" $LINENO $FUNCNAME  
-    dbgecho "replace str from end: ${str/%ld/lllllld}" $LINENO $FUNCNAME  
-    dbgecho "default value (unset): ${unset-"hellounset"}" $LINENO $FUNCNAME
+    dbgecho "substr to end: ${str:6}"
+    dbgecho "slice substr: ${str:6:3}"    ## the same as: echo | awk '{printf substr("'"$str"'", 3, 4) "\n"}'
+                                          ## take note: echo is used to generate a fake input so as not to offer a file to awk
+                                          ## take note: "'"$str"'"
+    dbgecho "substr from backend: ${str:(-6)}"
+    dbgecho "subtract paras to the end (*): ${*:3}"  
+    ## dbgecho "subtract para (@): ${@:3}"  ## look at the difference in the stdout :) 
+    dbgecho "subtract paras like slice: ${*:3:2}"  
+    dbgecho "cut str from left (not greedy): ${str#h*l}"  
+    dbgecho "cut str from left (greedy): ${str##h*l}"  
+    dbgecho "cut str from right (not greedy): ${str%l*}"  
+    dbgecho "cut str from right (greedy): ${str%%l*}"  
+    dbgecho "replace str once: ${str/l/o}"  
+    dbgecho "replace str all: ${str//l/o}"  
+    dbgecho "replace str from head: ${str/#he/she}"  
+    dbgecho "replace str from end: ${str/%ld/lllllld}"  
+    dbgecho "default value (unset): ${unset-"hellounset"}"
     local nullvar
-    dbgecho "default value (null): ${nullvar:-`whoami`}" $LINENO $FUNCNAME  ## it can be used to check parameter
-                                                                            ## it you want to assign at the same time
-                                                                            ## use '=' or ':=' instead
+    dbgecho "default value (null): ${nullvar:-`whoami`}"  ## it can be used to check parameter
+                                                          ## it you want to assign at the same time
+                                                          ## use '=' or ':=' instead
     : ${notset:="helloset"}     ## the appearance of ':' is important
-    dbgecho "default value (unset, assigned): $notset" $LINENO $FUNCNAME
+    dbgecho "default value (unset, assigned): $notset"
 
     local arr=( "ab" "abcd" "abcde" )
-    dbgecho "length of the array: ${#arr[*]}" $LINENO $FUNCNAME
-    dbgecho "length of the first item of the array: ${#arr}" $LINENO $FUNCNAME
+    dbgecho "length of the array: ${#arr[*]}"
+    dbgecho "length of the first item of the array: ${#arr}"
 
     local var=point
     local point="in-direct-value"
-    dbgecho "in-direct reference: $( eval echo \$$var )" $LINENO $FUNCNAME
-    dbgecho "in-direct reference: ${!var}" $LINENO $FUNCNAME
+    dbgecho "in-direct reference: $( eval echo \$$var )"
+    dbgecho "in-direct reference: ${!var}"
 
     (( likeC=1<3?11:22 ))   ## only apply to algorith
-    dbgecho "like C style: $likeC" $LINENO $FUNCNAME
+    dbgecho "like C style: $likeC"
 
     ## A comment trick: 
     ## : "
@@ -192,14 +207,14 @@ test_string()
     ## "
 
     for a; do   ## by default, the list is #@
-        dbgecho "$a " $LINENO $FUNCNAME
+        dbgecho "$a "
     done
 
 }
 
 
 test_op() {
-    dbgecho "pwd: $PWD" $LINENO $FUNCNAME
+    dbgecho "pwd: $PWD"
     op="ls"
     $op > "tmp.txt"
 }
