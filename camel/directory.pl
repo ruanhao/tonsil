65911c4e93e55de14a628969864106109c48b51c
Hao Ruan <ruanhao1116@gmail.com>
2013-05-24 17:37:26 +0800

    update

--- camel/directory.pl
+++ camel/directory.pl
@@ -40,13 +40,39 @@ use CamelUtil;
 ## those found in UNIX shells.
 &test_file_exist;
 
+## When a test is performed, Perl uses the 'stat(3)' system call to store the complete list 
+## of information from the 'inode'. If multiple tests are needed for a particular file, 
+## then 'stat' is called each time, unless _ is used. Using _ in a script tells Perl to use 
+## the 'inode' information from the last call. This both simplifies the code and provides a performance 
+## benefit by avoiding repeated system calls to 'stat'
+&test_stat;
 
+## Test permissions
+## Some operators are not limited to true/false return values. With the -s operator, the 
+## size of a file is returned.
+&test_permission;
 
+## File-name globbing is a method Perl uses for getting directory contents.
+## With globbing, a file-name pattern is specified in angle brackets. The pattern can 
+## contain shell wildcards, such as * or ?. When executed, Perl opens a shell in the background 
+## and assigns the pattern to it. The shell then resolves the pattern and returns the list of 
+## matching file names to Perl.
+&test_glob;
 
 
 
 
 ### Inner Functions
+
+sub test_permission {
+    my $file = "./hashfile.txt";
+    if (-e $file) {
+        &CamelUtil::dbgprint("$file is readable")   if (-r _);
+        &CamelUtil::dbgprint("$file is writable")   if (-w _);
+        &CamelUtil::dbgprint("$file is executable") if (-x _);
+    }
+    &CamelUtil::dbgprint("The size of $file: " . (-s _) . " bytes");    ## Note this
+}
 
 sub test_file_exist {
     my $file = "./hashfile.txt";
@@ -54,5 +80,34 @@ sub test_file_exist {
         &CamelUtil::dbgprint("$file exists");
     } else {
         &CamelUtil::dbgprint("$file not exists");
+    }
+}
+
+sub test_stat {
+    my $file = "./hashfile.txt";
+    -e $file or die "File does not exist. \n";
+    -w _ or die "File is not writable. \n";
+    &CamelUtil::dbgprint("$file exists and is writeable.");
+
+    ($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $size, $atime, $mtime, 
+     $ctime, $blksize, $blocks) = stat $file or die "Unable to stat $file: $!\n";
+    &CamelUtil::dbgprint("dev:               $dev");
+    &CamelUtil::dbgprint("inode:             $ino");
+    &CamelUtil::dbgprint("mode:              $mode");
+    &CamelUtil::dbgprint("# of links:        $nlink");
+    &CamelUtil::dbgprint("uid:               $uid");
+    &CamelUtil::dbgprint("gid:               $gid");
+    &CamelUtil::dbgprint("rdev:              $rdev");
+    &CamelUtil::dbgprint("size:              $size");
+    &CamelUtil::dbgprint("last access time:  $atime");
+    &CamelUtil::dbgprint("last modify time:  $mtime");
+    &CamelUtil::dbgprint("inode change time: $ctime");
+    &CamelUtil::dbgprint("block size:        $blksize");
+    &CamelUtil::dbgprint("blocks:            $blocks");
+}
+
+sub test_glob {
+    for (sort <./*>) {  ## Note this
+        &CamelUtil::dbgprint;
     }
 }
