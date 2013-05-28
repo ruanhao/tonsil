b17a7ae0cb0ad79f3320a8be1e8e934273e5bfb4
Hao Ruan <ruanhao1116@gmail.com>
2013-05-28 15:59:06 +0800

    add artvin

---
+++ artvin/user_default.erl
@@ -0,0 +1,8 @@
+-module(user_default).
+-compile(export_all).
+
+get_epoch() -> 
+    calendar:datetime_to_gregorian_seconds(calendar:universal_time())-719528*24*3600.
+
+get_now() -> 
+    calendar:now_to_local_time(now()).
